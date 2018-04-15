2920ad7ec9a0767ff4ff9c33bd63dcae8df6fb64
Emile <emile@ecspresso.com>
2018-04-15 17:50:43 +0200

    Added credential test

--- Office 365/Install O365 - Save credentials.ps1
+++ Office 365/Install O365 - Save credentials.ps1
@@ -1,4 +1,4 @@
-# Code from http://www.expta.com/2017/03/how-to-self-elevate-powershell-script.html
+ # Code from http://www.expta.com/2017/03/how-to-self-elevate-powershell-script.html
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
@@ -17,6 +17,23 @@ if(-not (Test-Path -path $path)) {
 Install-Module AzureAD
 
 $credentials = Get-Credential -Message "Enter your Office 365 credentials. These will be saved for later use."
+$credentials | Export-Clixml -Path $path\o365credentials.xml
+
+while($true) {
+    try {
+        $success = $true
+        $credential = Import-CliXML -Path $path\o365credentials.xml
+        Connect-AzureAD -Credential $credential > $null
+    } catch {
+        $success = $false
+        Write-Host "Connection failed. Please update your credentials."
+        $credential = Get-Credential -Message "Enter your Office 365 credentials. These will be saved for later use."
+    }
+
+    if ($success) {
+        break
+    }
+}
 
 $credentials | Export-Clixml -Path $path\o365credentials.xml
 
