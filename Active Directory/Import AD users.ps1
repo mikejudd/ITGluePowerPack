9248887ef002d49bac691691bdd38791d8d41037
ecspresso <emile@ecspresso.com>
2018-05-11 12:06:26 +0200

    Building data placeholder

--- Active Directory/Import AD users.ps1
+++ Active Directory/Import AD users.ps1
@@ -1,3 +1,11 @@
-get-aduser -Filter * | ForEach-Object {
-	
+$users = New-Object System.Collections.ArrayList
+
+Get-ADUser -Filter * -Property 'msDS-UserPasswordExpiryTimeComputed' | ForEach-Object {
+    $users.add((
+        New-Object –TypeName PSObject –Property (@{
+            'GivenName'=$_.GivenName;
+            'Surname'=$_.Surname;
+            'ExpiryDate' = ($_.'msDS-UserPasswordExpiryTimeComputed' | Select-Object @{Name='ExpiryDate';Expression={[datetime]::FromFileTime($_.'msDS-UserPasswordExpiryTimeComputed')}}).ExpiryDate
+        })
+    )) > $null
 }
