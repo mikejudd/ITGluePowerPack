9ec87029bccb93e1abe2228404a0089f7b9196b8
ecspresso <emile@ecspresso.com>
2018-06-01 10:21:20 +0200

    Improvements

--- Office 365/Import Office365 contacts - automated.ps1
+++ Office 365/Import Office365 contacts - automated.ps1
@@ -1,67 +1,9 @@
-function importO365Emails {
-    param (
-        [int]$organisationid
-    )
-    
-
-    # Get all contacts from ITGlue
-    $ITGlueContacts = ((Get-ITGlueContacts -page_size ((Get-ITGlueContacts).meta.'total-count')).data | Where-Object {$_.attributes.'organization-id' -eq $organisationid})
-
-
-    Get-AzureADUser | ForEach-Object {
-        if($_.AssignedLicenses.skuid -eq $null) {
-            # Remove "return" to import unlicensed users.
-            return
-        } elseif($ITGlueContacts.attributes.'contact-emails'.value -contains $_.UserPrincipalName) {
-            # Skip existing emails
-            return
-        }
-
-        $currentUser = $_
-
-        # Clean up name
-        if($currentUser.DisplayName.Split().Count -gt 1) {
-            $firstname = $currentUser.DisplayName.Replace($currentUser.DisplayName.Split()[$currentUser.DisplayName.Split().Count - 1], "")
-            $lastname = $currentUser.DisplayName.Split()[-1]
-            if($firstname.EndsWith(" ")) {
-                $firstname = $firstname.Substring(0, $firstname.Length - 1)
-            }
-        } else {
-            $firstname = $currentUser.DisplayName
-            $lastname = ""
-        }
-        
-        $body = @{
-            organization_id = $organisationid
-            data = @{
-                type = 'contacts'
-                attributes = @{
-                    first_name = $firstname
-                    last_name = $lastname
-                    notes = "Office 365 user"
-                    contact_emails = @(
-                        @{
-                            value = $currentUser.UserPrincipalName
-                            label_name = "Work"
-                            primary = $true
-                        }
-                    )
-                }
-            }
-        }
-
-        New-ITGlueContacts -data $body
-    }
-}
-
-if(-not (Get-Module -ListAvailable -Name AzureAD)) {
-    Write-Host "Please run the Office 365 install script first."
-    $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') > $null
-    exit
-}
-
-
-$path = "$env:USERPROFILE\UpstreamPowerPack"
+param (
+    [int]$organisationid,
+    [string]$path = "$env:USERPROFILE\UpstreamPowerPack",
+    [string]$username,
+    [string]$password
+)
 
 if(-not (Test-Path -path $path\o365credentials.xml)) {
     # If you did not save your Office 365 credentials via installation script, you need to enter them here.
@@ -72,7 +14,7 @@ if(-not (Test-Path -path $path\o365credentials.xml)) {
 
 
 
-    
+
     if($username -eq "YOUR OFFICE 365 EMAIL GOES HERE") {
         exit
     } else {
@@ -86,4 +28,59 @@ if(-not (Test-Path -path $path\o365credentials.xml)) {
 }
 
 
-#importO365Emails -organisationid 2504761
+if(-not (Get-Module -ListAvailable -Name AzureAD)) {
+    Write-Host "Please run the Office 365 install script first."
+    $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') > $null
+    exit
+}
+
+
+
+# Get all contacts from ITGlue
+$ITGlueContacts = ((Get-ITGlueContacts -page_size ((Get-ITGlueContacts).meta.'total-count')).data | Where-Object {$_.attributes.'organization-id' -eq $organisationid})
+
+
+Get-AzureADUser | ForEach-Object {
+    if($_.AssignedLicenses.skuid -eq $null) {
+        # Remove "return" to import unlicensed users.
+        return
+    } elseif($ITGlueContacts.attributes.'contact-emails'.value -contains $_.UserPrincipalName) {
+        # Skip existing emails
+        return
+    }
+
+    $currentUser = $_
+
+    # Clean up name
+    if($currentUser.DisplayName.Split().Count -gt 1) {
+        $firstname = $currentUser.DisplayName.Replace($currentUser.DisplayName.Split()[$currentUser.DisplayName.Split().Count - 1], "")
+        $lastname = $currentUser.DisplayName.Split()[-1]
+        if($firstname.EndsWith(" ")) {
+            $firstname = $firstname.Substring(0, $firstname.Length - 1)
+        }
+    } else {
+        $firstname = $currentUser.DisplayName
+        $lastname = ""
+    }
+
+    $body = @{
+        organization_id = $organisationid
+        data = @{
+            type = 'contacts'
+            attributes = @{
+                first_name = $firstname
+                last_name = $lastname
+                notes = "Office 365 user"
+                contact_emails = @(
+                    @{
+                        value = $currentUser.UserPrincipalName
+                        label_name = "Work"
+                        primary = $true
+                    }
+                )
+            }
+        }
+    }
+
+    New-ITGlueContacts -data $body
+}
